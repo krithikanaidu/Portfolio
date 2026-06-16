fb2f095faf7aa91bf325e65b489d341c72cdded8
krithikanaidu <virtualshakti05@gmail.com>
2026-06-17 01:28:15 +0530

    removed unnecessary files

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -3,8 +3,23 @@ import './App.css'
 function App() {
   return (
     <div className="center">
-      <h1>Hello there~</h1>
+      <div className="title">
+        <h1>This is my portfolio.</h1>
+      </div>
+
+    <div className="content">
+      <article>
+        <h1>My First Component</h1>
+        <ol>
+          <li>Components : UI Building blocks</li>
+          <li>Components are integral part in react</li>
+          <li>Using a Component</li>
+        </ol>
+      </article>
     </div>
+    </div>
+
+    
   );
 }
 
